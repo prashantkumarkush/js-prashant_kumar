9cd06ed6a9a8345813f2d0b95c2d9c2dafa6e78f
prashant kumar <prashant9102006@gmail.com>
2024-07-19 18:24:30 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3)  //we are using node js , not browser
+
+console.log(3
+    +3
+)   //code readiblity should  be high
+
+console.log("prashant")
+
+
+//datatype
+
+let name ="prashant"   //string
+let age = 20   //int
+let isLoggedIn = false //boolean
+let state;
+
+//number => 2 is power 53
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol unique 
+
+
+
+// object 
+
+
+console.log(typeof undefined)  //nudefined
+console.log(typeof null)  //object
