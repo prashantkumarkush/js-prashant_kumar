88f2a8e998cb14f2fa9dd3f9d053e16cc4bae113
prashant kumar <prashant9102006@gmail.com>
2024-07-19 11:40:53 +0000

    dataType

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144334
+let accountEmail = "prashantk963@gmail.com"
+var accountPassword = "122345"
+accountCity = "ARA"  //not variable use is correct but not good it is.
+ let accountState;
+
+
+// accountId = 2  // not allowed changed
+
+
+
+accountEmail = "hc@p.com"
+accountPassword = "3243"
+accountCity = "uttrakhand"
+
+console.log(accountId);
+
+/*
+preefer not to use var 
+because of issue in block and  functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
