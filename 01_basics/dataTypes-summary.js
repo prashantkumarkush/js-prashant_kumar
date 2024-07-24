793409e736af764792a6c6ef855cea8e894239ea
prashant kumar <prashant9102006@gmail.com>
2024-07-24 18:10:58 +0000

    boject2

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -14,7 +14,12 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
+
+
+
 // reference (non primitive)
+
+
 
 //Array, Objects, Functions
 
@@ -31,3 +36,27 @@ const myfunction = function(){
 console.log(typeof heros);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack(primitive), Heap(non-Premitive)
+
+let mybgminame = "omegaPrashant"
+
+let anothername = mybgminame
+anothername = "pandit"
+
+console.log(mybgminame);
+console.log(anothername);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+let userTwo = userOne
+
+userTwo = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
