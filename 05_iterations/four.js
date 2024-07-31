ff28e46a1738f81a34abdc22302168b315f1a5b0
prashant kumar <prashant9102006@gmail.com>
2024-07-31 03:02:11 +0000

    four

---
+++ 05_iterations/four.js
@@ -0,0 +1,38 @@
+const myObject = {
+    js: 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+// for in
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for(const key in programming){
+    // console.log(programming[key]);
+}
+
+
+
+// const map = new Map()
+// map.set('IN', "india")
+// map.set('USA', "united state of america")
+// map.set('fr', "france")
+
+// for (const key in map) {
+//   console.log(key);
+        
+//     }
+
+//no print value
+
+
+
+
+
+
